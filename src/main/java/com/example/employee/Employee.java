97f12c9b468866c155f7d1e14d653f33e95cae9c
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-06-25 13:45:45 +0530

    Update Employee.java

--- src/main/java/com/example/employee/Employee.java
+++ src/main/java/com/example/employee/Employee.java
@@ -3,52 +3,54 @@ package com.example.employee;
 
 public class Employee {
 
-private int employeeId; 
-private String employeeName;
+    private int employeeId;
 
-private String email;
+    private String employeeName;
 
-private String department;
+    private String email;
 
-public Employee(int employeeld, String employeeName, String email, String department) {
-    this.employeeId = employeeId;
-    this.employeeName =employeeName; 
-    this.email =email;
-    this.department = department;
+    private String department;
 
-}
+   public Employee(int employeeId, String employeeName, String email, String department)
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.email = email;
+        this.department = department;
 
-public int getEmployeeId() {
-    return employeeld;
-}
+    }
 
-public void setEmployeeId(int employeeId) { 
-    this.employeeId = employeeId;
+    public int getEmployeeId() {
 
-}
+        return employeeId;
+    }
 
-public String getEmployeeName() {
-    return employeeName;
-}
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
 
+    }
 
-public void setEmployeeName(String employeeName) {
-    this.employeeName = employeeName;
-}
+    public String getEmployeeName() {
+        return employeeName;
+    }
 
-public String getEmail() {
-     return email;
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
 
-}
-public void setEmail(String email) {
-    this.email= email;
-}
+    public String getEmail() {
+        return email;
 
-public String getDepartment() {
-    return department;
-}
+    }
 
-public void setDepartment(String department) {
-    this.department = department;
-}
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
