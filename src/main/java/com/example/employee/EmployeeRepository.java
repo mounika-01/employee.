5cab50ef5a7c9b007e3b65de1032687499723afd
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-06-25 13:46:50 +0530

    Update EmployeeRepository.java

--- src/main/java/com/example/employee/EmployeeRepository.java
+++ src/main/java/com/example/employee/EmployeeRepository.java
@@ -6,14 +6,14 @@ import java.util.*;
 import com.example.employee.Employee;
 
 public interface EmployeeRepository {
-    
+
     ArrayList<Employee> getEmployees();
-    
+
     Employee getEmployeeById(int employeeId);
-    
+
     Employee addEmployee(Employee employee);
 
-Employee updateEmployee(int employeeId, Employee employee);
+    Employee updateEmployee(int employeeId, Employee employee);
 
-void deleteEmployee(int employeeId)
+    void deleteEmployee(int employeeId);
 }
